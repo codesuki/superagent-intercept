09e7dccb97d3a1704c18816e99482b4927992ad1
codesuki <marschik_neri@cyberagent.co.jp>
2015-03-19 18:22:01 +0900

    fixed mistake adding the intercept globally instead of to the request

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ var request = require('superagent');
 function Intercept() {
 	var callbacks = Array.prototype.slice.call(arguments);
 	return function(req) {
-		var callback = request.Request.prototype.callback;
-		request.Request.prototype.callback = function(err, res) {
+		var callback = req.callback;
+		req.callback = function(err, res) {
 			callbacks.forEach(function(e) { e.call(req, err, res); });
 			callback.call(req, err, res);
 		};
